91730964a1f7e5a37eff504f8477265d9357a6e5
Felippe Nardi <felippe@nardi.me>
2018-01-24 17:52:41 -0200

    Mockup calculator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Button, Header, Form } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
+    const timeOptions = [
+      { text: 'horas', value: 'hours' },
+      { text: 'minutos', value: 'minutes' },
+    ];
+
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header as="h1">
+          Descubra o que dá pra ser feito com o tempo que você gasta na tarefa mais chata do mundo
+        </Header>
+        <Form size="massive">
+          <Form.Input label='Que tarefa é essa?' type='text' />
+          <Form.Group inline>
+            <Form.Input width="2" type='number' value="4" />
+            <Form.Select fluid options={timeOptions} value="hours" />
+            <Form.Input width="2" type='number' value="2" />
+            <Form.Field>
+              <p>vezes por mês</p>
+            </Form.Field>
+          </Form.Group>
+        </Form>
+        <Button color="yellow">Calcular</Button>
       </div>
     );
   }
