9aae294a2a4afe5ade68f63cf8288b27a1ec4ee8
Felippe Nardi <felippe@nardi.me>
2018-01-26 13:19:17 -0200

    Generate sentences based on closest time

--- src/Step2.js
+++ src/Step2.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Button, Header, Form, Grid } from 'semantic-ui-react'
+import sentences from './sentences';
 
 class Step2 extends Component {
   constructor(props) {
@@ -7,18 +8,52 @@ class Step2 extends Component {
 
     this.goToStep = this.goToStep.bind(this);
     this.totalTime = this.totalTime.bind(this);
+    this.totalInHours = this.totalInHours.bind(this);
+    this.renderSentence = this.renderSentence.bind(this);
   };
 
   goToStep(step) {
     this.props.changeStep(step);
   }
 
-  totalTime() {
+  totalInHours() {
     const { time, unit, repetitions } = this.props.task;
     const AN_YEAR_IN_MONTHS = 12;
-
-    const timeInHours = unit === 'hours' ? time : (time / 60).toFixed();
+    const timeInHours = unit === 'hours' ? time : (time / 60);
     return timeInHours * repetitions * AN_YEAR_IN_MONTHS;
+  }
+
+  totalTime() {
+    let result;
+    const hours = this.totalInHours();
+
+    if (hours >= 1) {
+      result = `${(hours).toFixed()} horas.`;
+    } else {
+      result = `${hours * 60} minutos.`
+    }
+
+    return result;
+  }
+
+  renderSentence() {
+    let sentence;
+    const totalInHours = this.totalInHours();
+
+    return sentences.reduce((finalSentence, currentSentence) => {
+      let sentence;
+
+      if (finalSentence.hours <= totalInHours &&
+        finalSentence.hours > currentSentence.hours ) {
+        sentence = finalSentence;
+      } else if (currentSentence.hours <= totalInHours) {
+        sentence = currentSentence;
+      } else {
+        sentence = finalSentence;
+      }
+
+      return sentence;
+    }).text;
   }
 
   render() {
@@ -36,7 +71,7 @@ class Step2 extends Component {
         </Header>
         <Header as="p" size="huge">
           <span className="score">
-          {this.totalTime()} horas.</span>
+          {this.totalTime()}</span>
         </Header>
 
         <br/>
@@ -47,7 +82,7 @@ class Step2 extends Component {
 
         <Header as="p" size="huge">
           <span className="score">
-            ler todos os livros do Game of Thrones.
+            {this.renderSentence()}.
           </span>
         </Header>
 
---
+++ src/sentences.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    hours: 72,
+    text: 'chegar de foguete até a Lua'
+  },
+  {
+    hours: 1.5,
+    text: 'carregar um iPhone'
+  },
+  {
+    hours: 0.16,
+    text: 'SpaceX pousar o primeiro estágio de um foguete'
+  },
+  {
+    hours: 720,
+    text: 'assistir à uma temporada de friends. Só uma'
+  },
+  {
+    hours: 200,
+    text: 'ler todos os livros do Game of Thrones'
+  },
+  {
+    hours: 240,
+    text: 'visitar todos os parques da Disney'
+  },
+  {
+    hours: 1000,
+    text: 'assistir todos os filmes do Harry Potter'
+  },
+  {
+    hours: 960,
+    text: 'escalar o Monte Everest'
+  },
+  {
+    hours: 5760,
+    text: 'assistir todos os jogos da Copa do Mundo de 2014'
+  },
+  {
+    hours: 5,
+    text: 'zerar o Super Mario Bros num Super Nintendo'
+  },
+  {
+    hours: 5,
+    text: 'zerar o Super Mario Bros num Super Nintendo'
+  },
+];
